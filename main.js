87fe23e92e3acf5138c606fad5a418f1a3396eb1
Desforges Thomas <69684754+tosbas@users.noreply.github.com>
2023-11-22 21:57:52 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,63 +2,49 @@ import { Game, Player } from "./game.js";
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d', { willReadFrequently: true });
+
 const startBtn = document.querySelector('#startBtn');
+const setBtns = document.querySelectorAll("[id*=set]");
 
-const btnSetPlayerRadius = document.querySelector("#btnSetPlayerRadius");
-const btnSetPlayerMaxLife = document.querySelector("#btnSetPlayerMaxLife");
-const btnSetEnemieMaxEnemies = document.querySelector("#btnSetEnemieMaxEnemies");
-const btnSetPlayerShootMass = document.querySelector("#btnSetPlayerShootMass");
-const btnSetPlayerShootRadius = document.querySelector("#btnSetPlayerShootRadius");
-const btnSetEnemietimeEnemyAppear = document.querySelector("#btnSetEnemietimeEnemyAppear");
-const btnSetParticuleRadius = document.querySelector("#btnSetParticuleRadius");
-const btnSetParticuleMax = document.querySelector("#btnSetParticuleMax");
-const btnSetParticleMass = document.querySelector("#btnSetParticleMass");
-const btnSetParticleVelocityMagnitude = document.querySelector("#btnSetParticleVelocityMagnitude");
-const btnSetMinAngleSpread = document.querySelector("#btnSetMinAngleSpread");
-const btnSetMaxAngleSpread = document.querySelector("#btnSetMaxAngleSpread");
-const btnSetVelocityReductionFactor = document.querySelector("#btnSetVelocityReductionFactor");
-const btnSetDampingFactor = document.querySelector("#btnSetDampingFactor");
-const btnSetOverlapReductionFactor = document.querySelector("#btnSetOverlapReductionFactor");
-
+const menuItem = document.querySelector(".menu-item");
+const menuOpen = document.querySelector("#menu-btn");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
 
 const player = new Player(canvas, ctx, "white");
 const game = new Game(canvas, ctx, startBtn, player);
 
 game.run();
 
+/**
+ * Ecoute le changement de valeur sur un button 
+ * @param {object} button 
+ */
 const setParameterChange = (button) => {
     button.addEventListener("change", (e) => {
         if (!isNaN(e.target.value)) {
 
-            const buttonId = e.target.id;
+            const methode = e.target.id;
+            const value = parseFloat(e.target.value);
 
-            const methode = buttonId.split('btn')[1];
-
-            const methodeName = methode.charAt(0).toLowerCase() + methode.slice(1);
-
-            game[methodeName](e.target.value);
+            game[methode](value);        
         }
     });
 };
 
-setParameterChange(btnSetPlayerRadius);
-setParameterChange(btnSetPlayerMaxLife);
-setParameterChange(btnSetEnemieMaxEnemies);
-setParameterChange(btnSetPlayerShootMass);
-setParameterChange(btnSetPlayerShootRadius);
-setParameterChange(btnSetEnemietimeEnemyAppear);
-setParameterChange(btnSetParticuleRadius);
-setParameterChange(btnSetParticuleMax);
-setParameterChange(btnSetParticleMass);
-setParameterChange(btnSetParticleVelocityMagnitude);
-setParameterChange(btnSetMinAngleSpread);
-setParameterChange(btnSetMaxAngleSpread);
-setParameterChange(btnSetVelocityReductionFactor);
-setParameterChange(btnSetDampingFactor);
-setParameterChange(btnSetOverlapReductionFactor);
+for (const button of setBtns) {
+    setParameterChange(button);
+}
+
+menuOpen.addEventListener("click", () => {
+    const computedStyle = window.getComputedStyle(menuItem);
+    const isBlock = computedStyle.display === "block";
+
+    menuOpen.innerHTML = isBlock ? "Ouvrir" : "Fermer";
+    menuItem.style.display = isBlock ? "none" : "block";
+});
+
+
 
 
